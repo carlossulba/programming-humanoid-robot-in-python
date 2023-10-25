323a7b52897e1921751545140e14fef26611c314
carlos.sulbaran <carlossulba000@gmail.com>
2023-10-25 19:51:55 +0200

    pid.py 1. implement control 2. adjust parameters

--- joint_control/pid.py
+++ joint_control/pid.py
@@ -34,10 +34,10 @@ class PIDController(object):
         self.e1 = np.zeros(size)
         self.e2 = np.zeros(size)
         # ADJUST PARAMETERS BELOW
-        delay = 0
-        self.Kp = 0
-        self.Ki = 0
-        self.Kd = 0
+        delay = 3
+        self.Kp = 1
+        self.Ki = 0.1
+        self.Kd = 0.1
         self.y = deque(np.zeros(size), maxlen=delay + 1)
 
     def set_delay(self, delay):
@@ -53,6 +53,19 @@ class PIDController(object):
         @return control signal
         '''
         # YOUR CODE HERE
+        # compute error
+        self.y.append(sensor)
+        if np.size(self.y[0]) == 22:
+            e = target - self.y[-1]
+        else:
+            e = target
+
+        # compute signal
+        self.u = self.u + (self.Kp + self.Ki*self.dt + self.Kd/self.dt)*e - (self.Kp + 2*self.Kd/self.dt)*self.e1 + (self.Kd/self.dt)*self.e2
+
+        # update errors
+        self.e2 = self.e1
+        self.e1 = e
 
         return self.u
 
@@ -85,5 +98,5 @@ class PIDAgent(SparkAgent):
 
 if __name__ == '__main__':
     agent = PIDAgent()
-    agent.target_joints['HeadYaw'] = 1.0
+    agent.target_joints['HeadYaw'] = 1
     agent.run()
