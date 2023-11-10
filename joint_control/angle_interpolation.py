8fce7c19ada7d85f83a3dbca3e78c120c4845fa6
carlos.sulbaran <carlossulba000@gmail.com>
2023-11-10 05:51:09 +0100

    angle_interpolation.py: bezier interpolation ignoring non linearity

--- joint_control/angle_interpolation.py
+++ joint_control/angle_interpolation.py
@@ -2,7 +2,7 @@
 
 * Tasks:
     1. complete the code in `AngleInterpolationAgent.angle_interpolation`,
-       you are free to what is meant with angle interp splines interploation or Bezier interploation,
+       you are free to use splines interploation or Bezier interploation,
        but the keyframes provided are for Bezier curves, you can simply ignore some data for splines interploation,
        please refer data format below for details.
     2. try different keyframes from `keyframes` folder
@@ -19,9 +19,19 @@
     # preceding the point, the second describes the curve following the point.
 '''
 
-
 from pid import PIDAgent
 from keyframes import hello
+
+
+
+
+def calc_bezier(t, p0, p1, p2, p3):
+    return (
+            ((1 - t) ** 3 * p0)
+            + (3 * (1 - t) ** 2 * t * p1)
+            + (3 * (1 - t) * (t ** 2) * p2)
+            + ((t ** 3) * p3)
+    )
 
 
 class AngleInterpolationAgent(PIDAgent):
@@ -32,21 +42,68 @@ class AngleInterpolationAgent(PIDAgent):
                  sync_mode=True):
         super(AngleInterpolationAgent, self).__init__(simspark_ip, simspark_port, teamname, player_id, sync_mode)
         self.keyframes = ([], [], [])
+        self.born_time = self.perception.time
 
     def think(self, perception):
         target_joints = self.angle_interpolation(self.keyframes, perception)
-        target_joints['RHipYawPitch'] = target_joints['LHipYawPitch'] # copy missing joint in keyframes
         self.target_joints.update(target_joints)
         return super(AngleInterpolationAgent, self).think(perception)
 
     def angle_interpolation(self, keyframes, perception):
         target_joints = {}
-        # YOUR CODE HERE
+        current_time = perception.time - self.born_time
 
+        # if current_time > 6:
+        #     self.born_time = self.perception.time
+        #     self.keyframes = hello()
+
+        names, times, keys = keyframes
+
+        for i, name in enumerate(names):
+            # Find the correct time interval
+            for j in range(len(times[i]) - 1):
+                if times[i][j] <= current_time <= times[i][j + 1]:
+                    # Calculate Bezier control points
+                    p0 = keys[i][j][0]
+                    p1 = p0 + keys[i][j][2][2]
+                    p3 = keys[i][j + 1][0]
+                    p2 = p3 + keys[i][j + 1][1][2]
+                    t = (current_time - times[i][j]) / (times[i][j + 1] - times[i][j])
+                    target_joints[name] = calc_bezier(t, p0, p1, p2, p3)
+                    break
+                elif current_time < times[i][0]:
+                    # In case we are before the first keyframe
+                    if name in perception.joint:
+                        p0 = perception.joint[name]
+                    else:
+                        p0 = keys[i][0][0]
+                    p3 = keys[i][0][0]
+
+                    p1 = p0 + 0
+                    p2 = p3 + 0
+                    t = current_time / times[i][0]
+                    target_joints[name] = calc_bezier(t, p0, p1, p2, p3)
+                    break
+
+        # Copy joint angle from one side to the other if necessary
+        if 'LHipYawPitch' in target_joints:
+            target_joints['RHipYawPitch'] = target_joints['LHipYawPitch']
 
         return target_joints
+
+# from keyframes import leftBackToStand
+# from keyframes import leftBellyToStand
+# from keyframes import rightBackToStand
+# from keyframes import rightBellyToStand
+# from keyframes import wipe_forehead
 
 if __name__ == '__main__':
     agent = AngleInterpolationAgent()
     agent.keyframes = hello()  # CHANGE DIFFERENT KEYFRAMES
+    # agent.keyframes = leftBackToStand()
+    # agent.keyframes = leftBellyToStand()
+    # agent.keyframes = rightBackToStand()
+    # agent.keyframes = rightBellyToStand()
+    # agent.keyframes = wipe_forehead()
     agent.run()
+
