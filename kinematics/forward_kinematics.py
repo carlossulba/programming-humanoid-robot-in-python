0ae380770db3d22789aa2f525e3f6a6b8af060f1
carlos.sulbaran <carlossulba000@gmail.com>
2023-11-26 22:27:34 +0100

    forward_kinematics.py

--- kinematics/forward_kinematics.py
+++ kinematics/forward_kinematics.py
@@ -19,6 +19,8 @@
 # add PYTHONPATH
 import os
 import sys
+import numpy as np
+
 sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'joint_control'))
 
 from numpy.matlib import matrix, identity
@@ -36,9 +38,39 @@ class ForwardKinematicsAgent(PostureRecognitionAgent):
         self.transforms = {n: identity(4) for n in self.joint_names}
 
         # chains defines the name of chain and joints of the chain
-        self.chains = {'Head': ['HeadYaw', 'HeadPitch']
-                       # YOUR CODE HERE
+        self.chains = {'Head': ['HeadYaw', 'HeadPitch'],
+                       'LArm': ['LShoulderPitch', 'LShoulderRoll', 'LElbowYaw', 'LElbowRoll'],
+                       'RArm': ['RShoulderPitch', 'RShoulderRoll', 'RElbowYaw', 'RElbowRoll'],
+                       'LLeg': ['LHipYawPitch', 'LHipRoll', 'LHipPitch', 'LKneePitch', 'LAnklePitch', 'LAnkleRoll'],
+                       'RLeg': ['RHipYawPitch', 'RHipRoll', 'RHipPitch', 'RKneePitch', 'RAnklePitch', 'RAnkleRoll']
                        }
+        self.joint_lengths = {'HeadYaw': (0.00, 0.00, 0.1265),
+                              'HeadPitch': (0.00, 0.00, 0.00),
+
+                              'LShoulderPitch': (0.00, 0.098, 0.1),
+                              'LShoulderRoll': (0.00, 0.00, 0.00),
+                              'LElbowYaw': (0.105, 0.015, 0.00),
+                              'LElbowRoll': (0.00, 0.00, 0.00),
+
+                              'RShoulderPitch': (0.00, -0.098, 0.1),
+                              'RShoulderRoll': (0.00, 0.00, 0.00),
+                              'RElbowYaw': (0.105, -0.015, 0.00),
+                              'RElbowRoll': (0.00, 0.00, 0.00),
+
+                              'LHipYawPitch': (0.00, 0.05, -0.085),
+                              'LHipRoll': (0.00, 0.00, 0.00),
+                              'LHipPitch': (0.00, 0.00, 0.00),
+                              'LKneePitch': (0.00, 0.00, -0.1),
+                              'LAnklePitch': (0.00, 0.00, -0.1029),
+                              'LAnkleRoll': (0.00, 0.00, 0.00),
+
+                              'RHipYawPitch': (0.00, -0.05, -0.085),
+                              'RHipRoll': (0.00, 0.00, 0.00),
+                              'RHipPitch': (0.00, 0.00, 0.00),
+                              'RKneePitch': (0.00, 0.00, -0.1),
+                              'RAnklePitch': (0.00, 0.00, -0.1029),
+                              'RAnkleRoll': (0.00, 0.00, 0.00),
+                              }
 
     def think(self, perception):
         self.forward_kinematics(perception.joint)
@@ -52,9 +84,43 @@ class ForwardKinematicsAgent(PostureRecognitionAgent):
         :return: transformation
         :rtype: 4x4 matrix
         '''
+
+        def rotation_x(angle):
+            """Create a rotation matrix for a rotation around the x-axis."""
+            cos, sin = np.cos(angle), np.sin(angle)
+            return np.array([[1, 0, 0, 0],
+                             [0, cos, -sin, 0],
+                             [0, sin, cos, 0],
+                             [0, 0, 0, 1]])
+
+        def rotation_y(angle):
+            """Create a rotation matrix for a rotation around the y-axis."""
+            cos, sin = np.cos(angle), np.sin(angle)
+            return np.array([[cos, 0, sin, 0],
+                             [0, 1, 0, 0],
+                             [-sin, 0, cos, 0],
+                             [0, 0, 0, 1]])
+
+        def rotation_z(angle):
+            """Create a rotation matrix for a rotation around the z-axis."""
+            cos, sin = np.cos(angle), np.sin(angle)
+            return np.array([[cos, sin, 0, 0],
+                             [-sin, cos, 0, 0],
+                             [0, 0, 1, 0],
+                             [0, 0, 0, 1]])
+
         T = identity(4)
         # YOUR CODE HERE
+        if 'Roll' in joint_name:
+            T = rotation_x(joint_angle)
+        elif 'Pitch' in joint_name:
+            T = rotation_y(joint_angle)
+        elif 'Yaw' in joint_name:
+            T = rotation_z(joint_angle)
 
+        # Store the offsets in the last column.
+        T[0:3, -1] = self.joint_lengths[joint_name]
+        # Where are the offsets of each joint? Last row or last column? I think column
         return T
 
     def forward_kinematics(self, joints):
@@ -64,12 +130,13 @@ class ForwardKinematicsAgent(PostureRecognitionAgent):
         '''
         for chain_joints in self.chains.values():
             T = identity(4)
-            for joint in chain_joints:
-                angle = joints[joint]
-                Tl = self.local_trans(joint, angle)
-                # YOUR CODE HERE
+            for joint_name in chain_joints:
+                if joint_name in joints.keys():
+                    joint_angle = joints[joint_name]
+                    Tl = self.local_trans(joint_name, joint_angle)
+                    T = np.dot(T, Tl)
+                    self.transforms[joint_name] = T
 
-                self.transforms[joint] = T
 
 if __name__ == '__main__':
     agent = ForwardKinematicsAgent()
